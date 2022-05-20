4f958480d8fff87fd54f64d2d24e33413bb01528
Marco Bertini <m.bertini92@gmail.com>
2022-05-21 00:07:06 +0200

    Started implementation of subroutine cell_reshape

--- src/main.f90
+++ src/main.f90
@@ -1,9 +1,11 @@
 program main
 
   use mod_error
+  use mod_p3d_t
   use mod_xyz_t
   use mod_read_xyz
   use mod_write_xyz
+  use mod_cell_reshape
 
   implicit none
 
@@ -11,7 +13,8 @@ program main
   character(120) :: prog_name
   character(120) :: fname
   type(xyz_t) :: xyz_input
-!  type(xyz_t) :: xyz_output
+  type(xyz_t) :: xyz_output
+  type(p3d_t) :: cell_center
 
   call get_command_argument(0,prog_name)
 
@@ -22,10 +25,12 @@ program main
 
   call get_command_argument(1,fname)
 
-  call read_xyz(trim(fname),xyz_input)
+  call read_xyz(trim(fname),xyz_input,cell_center)
   write(*,*) "Input:"
   call write_xyz(xyz_input)
-!  call cell_reshape()
+  write(*,*) "Cell center:"
+  write(*,*) cell_center%x, cell_center%y, cell_center%z
+  call cell_reshape(xyz_input,xyz_output,cell_center)
 !  call cell_saturate()
 
 contains
--- src/mod_cell_reshape.f90
+++ src/mod_cell_reshape.f90
@@ -1,5 +1,10 @@
 module mod_cell_reshape
 
+  use mod_parameters
+  use mod_error
+  use mod_p3d_t
+  use mod_xyz_t
+
   implicit none
   save
   private
@@ -8,10 +13,43 @@ module mod_cell_reshape
 
 contains
 
-subroutine cell_reshape(cell_in,cell_out)
+subroutine cell_reshape(cin,cout,center)
 
-  integer, intent(in)  :: cell_in
-  integer, intent(out) :: cell_out
+  type(xyz_t), intent(in)  :: cin
+  type(xyz_t), intent(out) :: cout
+  type(p3d_t), intent(in)  :: center
+  character(*), parameter :: my_name = "cell_reshape"
+  integer :: n
+  real(dbl) :: x_min
+  real(dbl) :: x_max
+  real(dbl) :: y_min
+  real(dbl) :: y_max
+  real(dbl) :: z_min
+  real(dbl) :: z_max
+  integer :: err_n
+  character(120) :: err_msg
+
+  n = cin%n
+  cout%n = n
+  allocate(cout%e(n),stat=err_n,errmsg=err_msg)
+  if (err_n /= 0) call error(my_name,err_msg)
+  allocate(cout%x(n),stat=err_n,errmsg=err_msg)
+  if (err_n /= 0) call error(my_name,err_msg)
+  allocate(cout%y(n),stat=err_n,errmsg=err_msg)
+  if (err_n /= 0) call error(my_name,err_msg)
+  allocate(cout%z(n),stat=err_n,errmsg=err_msg)
+  if (err_n /= 0) call error(my_name,err_msg)
+
+  x_min = minval(cin%x)
+  x_max = maxval(cin%x)
+  y_min = minval(cin%y)
+  y_max = maxval(cin%y)
+  z_min = minval(cin%z)
+  z_max = maxval(cin%z)
+
+  write(*,*) x_min, x_max
+  write(*,*) y_min, y_max
+  write(*,*) z_min, z_max
 
 end subroutine cell_reshape
 
