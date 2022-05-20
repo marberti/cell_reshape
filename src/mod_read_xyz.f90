490a97a9365fef3fc99d752a4f53e343d6a3f622
Marco Bertini <m.bertini92@gmail.com>
2022-05-21 00:02:38 +0200

    Now subroutine read_xyz reads center

--- src/mod_read_xyz.f90
+++ src/mod_read_xyz.f90
@@ -2,6 +2,7 @@ module mod_read_xyz
 
   use mod_parameters
   use mod_error
+  use mod_p3d_t
   use mod_xyz_t
   use mod_get_field
 
@@ -13,18 +14,23 @@ module mod_read_xyz
 
 contains
 
-subroutine read_xyz(fname,xyz)
+subroutine read_xyz(fname,xyz,center)
 
   character(*), intent(in) :: fname
   type(xyz_t), intent(out) :: xyz
+  type(p3d_t), intent(out) :: center
   character(*), parameter :: my_name = "read_xyz"
   integer, parameter :: fnumb = 200
   character(200) :: buff
   character(100) :: field
+  character(8) :: i_str
   integer :: i
   integer :: n
+  logical :: flag_found_center
   integer :: err_n
   character(120) :: err_msg
+
+  flag_found_center = .false.
 
   open(unit=fnumb,file=fname,status="old",action="read",&
     iostat=err_n,iomsg=err_msg)
@@ -60,10 +66,28 @@ subroutine read_xyz(fname,xyz)
     call get_field(buff,field,4,err_n,err_msg)
     if (err_n /= 0) call error(my_name,err_msg)
     read(field,*) xyz%z(i)
+    call get_field(buff,field,5,err_n,err_msg)
+    if (err_n == 0) then
+      if (trim(field) == "*") then
+        if (flag_found_center) call error(my_name,&
+          "specified more than one center in the input geometry")
+        center%x = xyz%x(i)
+        center%y = xyz%y(i)
+        center%z = xyz%z(i)
+        flag_found_center = .true.
+      else
+        write(i_str,"(I8)") i+2
+        i_str = adjustl(i_str)
+        call error(my_name,"unexpected input on line "//trim(i_str))
+      end if
+    end if
   end do
 
   close(unit=fnumb,iostat=err_n,iomsg=err_msg)
   if (err_n /= 0) call error(my_name,err_msg)
+
+  if (flag_found_center.eqv..false.) call error(my_name,&
+    "no center was specified in the input geometry")
 
 end subroutine read_xyz
 
