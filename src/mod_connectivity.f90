ce3a32e2925cc46ba9efe0a000c837ebdc80e44f
Marco Bertini <m.bertini92@gmail.com>
2022-05-26 23:01:28 +0200

    Now subroutine get_connectivity_matrix() uses mod_van_der_waals

--- src/mod_connectivity.f90
+++ src/mod_connectivity.f90
@@ -4,6 +4,7 @@ module mod_connectivity
   use mod_error
   use mod_p3d_t
   use mod_euclidean_distance
+  use mod_van_der_waals
 
   implicit none
   save
@@ -19,8 +20,8 @@ subroutine get_connectivity_matrix(e,x,y,z,dm,lm)
   real(dbl), dimension(:), intent(in) :: x
   real(dbl), dimension(:), intent(in) :: y
   real(dbl), dimension(:), intent(in) :: z
-  real(dbl), dimension(:,:), allocatable, intent(out) :: dm
-  logical, dimension(:,:), allocatable, intent(out) :: lm
+  real(dbl), dimension(:,:), intent(out) :: dm
+  logical, dimension(:,:), intent(out) :: lm
   character(*), parameter :: my_name = "get_connectivity_matrix"
   integer :: n
   integer :: i
@@ -29,23 +30,17 @@ subroutine get_connectivity_matrix(e,x,y,z,dm,lm)
   type(p3d_t) :: p2
   real(dbl) :: distance
   logical :: connection
-  integer :: err_n
-  character(120) :: err_msg
 
   n = size(e)
 
   if (size(x) /= n) call error(my_name,"mismatching input vectors sizes")
   if (size(y) /= n) call error(my_name,"mismatching input vectors sizes")
   if (size(z) /= n) call error(my_name,"mismatching input vectors sizes")
-
-  allocate(dm(n,n),stat=err_n,errmsg=err_msg)
-  if (err_n /= 0) then
-    call error(my_name,err_msg)
+  if ((size(dm,1) /= n).or.(size(dm,2) /= n)) then
+    call error(my_name,"mismatching dm matrix sizes")
   end if
-
-  allocate(lm(n,n),stat=err_n,errmsg=err_msg)
-  if (err_n /= 0) then
-    call error(my_name,err_msg)
+  if ((size(lm,1) /= n).or.(size(lm,2) /= n)) then
+    call error(my_name,"mismatching lm matrix sizes")
   end if
 
   dm = 0.0_dbl
@@ -60,7 +55,7 @@ subroutine get_connectivity_matrix(e,x,y,z,dm,lm)
       p2%y = y(j)
       p2%z = z(j)
       distance = compute_distance_3d(p1,p2)
-!      connection = check_vdw_bond(e(i),e(j),distance)
+      connection = check_vdw_bond(e(i),e(j),distance)
       dm(i,j) = distance
       dm(j,i) = distance
       lm(i,j) = connection
