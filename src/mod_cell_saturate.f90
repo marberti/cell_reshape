fac4cfab3b2098a3e9a02019dd6795a3478a35bb
Marco Bertini <m.bertini92@gmail.com>
2022-05-27 17:22:05 +0200

    Started implementation of hydrogen saturation logic

--- src/mod_cell_saturate.f90
+++ src/mod_cell_saturate.f90
@@ -3,6 +3,7 @@ module mod_cell_saturate
   use mod_parameters
   use mod_error
   use mod_cell_t
+  use mod_periodic_table
   use mod_logical
   use mod_connectivity
 
@@ -14,6 +15,8 @@ module mod_cell_saturate
 
 contains
 
+! Public !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 subroutine cell_saturate(cin,cout)
 
   type(cell_t), intent(in)  :: cin
@@ -21,7 +24,6 @@ subroutine cell_saturate(cin,cout)
   character(*), parameter :: my_name = "cell_saturate"
   real(dbl), dimension(:,:), allocatable :: dm
   logical, dimension(:,:), allocatable :: lm
-  integer, dimension(:), allocatable :: bonds
   integer :: i
   integer :: n
   integer :: err_n
@@ -35,9 +37,6 @@ subroutine cell_saturate(cin,cout)
   allocate(lm(n,n),stat=err_n,errmsg=err_msg)
   if (err_n /= 0) call error(my_name,err_msg)
 
-  allocate(bonds(n),stat=err_n,errmsg=err_msg)
-  if (err_n /= 0) call error(my_name,err_msg)
-
   ! Output is not a periodic cell, cell constants are meaningless
   cout%a = 0.0
   cout%b = 0.0
@@ -46,7 +45,7 @@ subroutine cell_saturate(cin,cout)
   call get_connectivity_matrix(cin%xyz%e,cin%xyz%x,cin%xyz%y,cin%xyz%z,dm,lm)
 
   do i = 1, n
-    bonds(i) = count_true(lm(:,i))
+    call atom_saturate(i,cin,lm)
   end do
 
   deallocate(dm,stat=err_n,errmsg=err_msg)
@@ -55,9 +54,81 @@ subroutine cell_saturate(cin,cout)
   deallocate(lm,stat=err_n,errmsg=err_msg)
   if (err_n /= 0) call error(my_name,err_msg)
 
-  deallocate(bonds,stat=err_n,errmsg=err_msg)
-  if (err_n /= 0) call error(my_name,err_msg)
-
 end subroutine cell_saturate
+
+! Private !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine atom_saturate(i,cin,lm)
+
+  integer, intent(in) :: i
+  type(cell_t), intent(in) :: cin
+  logical, dimension(:,:), intent(in) :: lm
+  character(*), parameter :: my_name = "atom_saturate"
+  integer :: bonds_done
+  integer :: bonds_max
+  real(dbl) :: dist
+
+  bonds_max = 4 ! FIXME
+  !bonds_max = pt_get_max_bonds(cin%xyz%e(i))
+  bonds_done = count_true(lm(:,i))
+
+  if (bonds_done >= bonds_max) return
+
+  dist = pt_get_r_mean("H ") + pt_get_r_mean(cin%xyz%e(i))
+
+  ! TODO remove
+  print *, cin%xyz%e(i), cin%xyz%x(i), cin%xyz%y(i), cin%xyz%z(i)
+  ! end
+
+  select case (bonds_max)
+  case (1)
+    call error(my_name,"bonds_max == 1 not implemented yet")
+  case (2)
+    call error(my_name,"bonds_max == 2 not implemented yet")
+  case (3)
+    call error(my_name,"bonds_max == 3 not implemented yet")
+  case (4)
+    call saturate_tetrahedral(bonds_done,dist)
+  case default
+    call error(my_name,"bonds_max > 4 not implemented yet")
+  end select
+
+  stop 33
+
+end subroutine atom_saturate
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine saturate_tetrahedral(bonds_done,d)
+
+  integer, intent(in) :: bonds_done
+  real(dbl), intent(in) :: d
+  character(*), parameter :: my_name = "saturate_tetrahedral"
+  real(dbl) :: theta
+  real(dbl) :: phi
+
+  select case (bonds_done)
+  case (0)
+    theta = 0.0_dbl
+    phi   = 0.0_dbl
+    print *, "H", d*cos(theta), d*sin(theta)*sin(phi), d*sin(theta)*cos(phi)
+    theta = 1.9106332356470423_dbl
+    phi   = 0.0_dbl
+    print *, "H", d*cos(theta), d*sin(theta)*sin(phi), d*sin(theta)*cos(phi)
+    theta = 1.9106332356470423_dbl
+    phi   = 2.0943951023931953_dbl
+    print *, "H", d*cos(theta), d*sin(theta)*sin(phi), d*sin(theta)*cos(phi)
+    theta = 1.9106332356470423_dbl
+    phi   = 4.1887902047863905_dbl
+    print *, "H", d*cos(theta), d*sin(theta)*sin(phi), d*sin(theta)*cos(phi)
+  case (1)
+    call error(my_name,"bonds_done == 1 not implemented yet")
+  case (2)
+    call error(my_name,"bonds_done == 2 not implemented yet")
+  case (3)
+    call error(my_name,"bonds_done == 3 not implemented yet")
+  end select
+
+end subroutine saturate_tetrahedral
 
 end module mod_cell_saturate
