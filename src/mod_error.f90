f19bec2698fd4ea0281be9db4a65aebcd1433e52
Marco Bertini <m.bertini92@gmail.com>
2022-06-15 13:10:23 +0200

    Changed output of subroutine error()

--- src/mod_error.f90
+++ src/mod_error.f90
@@ -13,7 +13,7 @@ subroutine error(proc_name,proc_msg)
   character(*), intent(in) :: proc_name
   character(*), intent(in) :: proc_msg
 
-  write(ERROR_UNIT,'(1X,A,": ",A)') proc_name, proc_msg
+  write(ERROR_UNIT,'(" ERR ",A,": ",A)') proc_name, proc_msg
   stop 1
 
 end subroutine error
